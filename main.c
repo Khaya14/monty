4630ff7cc010c8a9cfbfd47e99a91398b5d3888f
Khaya14 <khayashezi14@gmail.com>
2024-01-18 22:35:46 +0200

    Exec

--- main.c
+++ main.c
@@ -1,49 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
 
 bus_t bus = {NULL, NULL, NULL, 0};
 
-/**
- * main - serves as a monty code interpreter
- * 
- * @argc: the number of arguments
- * @argv: the monty file location
- * Return: 0 on success
- */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
-	char *content;
+	char *content = NULL;
 	FILE *file;
 	size_t t_size = 0;
-	ssize_t ssiz = 1;
 	stack_t *stack = NULL;
 	unsigned int count = 0;
 
-	if (argc != 2)
+	if (argc != 2) 
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	bus.file = file;
-	if (!file)
+
+	if (!file) 
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (ssiz > 0)
+
+	while ((content = malloc(t_size + 1)) != NULL && fgets(content, t_size + 1, file) != NULL) 
 	{
-		content = NULL;
-		ssiz = getline(&content, &t_size, file);
-		bus.content = content;
 		count++;
-		if (ssiz > 0)
-		{
-			execute(content, &stack, count, file);
-		}
+		execute(content, &stack, count, file);
 		free(content);
 	}
+
 	free_stack(stack);
 	fclose(file);
-	return (0);
+	return 0;
 }
+
